427c57d286c57961a13c3d77244962cd422b6f0a
coldicefisher <62789324+coldicefisher@users.noreply.github.com>
2026-03-04 22:46:50 -0800

    added openai support and baseline test case with 3.5. 4 models and above not implemented yet

--- autocoder/coding_environment/scratchpad.py
+++ autocoder/coding_environment/scratchpad.py
@@ -43,23 +43,25 @@ config = ChatGPTClientConfig(
     is_azure=False,
     api_base=None,
     available_models=None,
-    default_model=None,
+    default_model='gpt-3.5-turbo',
     config_file_path=None,
 )
 
 class CalculatorValidator(BaseValidator):
     def validate(self, input_data: str, output_data: str = "", *args, **kwargs) -> ValidationResult:
         try:
-            result = float(output_data)
+            float(output_data)
             return ValidationResult(is_valid=True)
-        except ValueError:
+        except (ValueError, TypeError):
             pass
-            # return ValidationResult(is_valid=False, errors=["Output data is not a number."])
-    
-        if isinstance(output_data, None):
+
+        if output_data is None:
             return ValidationResult(is_valid=True)
-        
-        return ValidationResult(is_valid=False, errors=["Output data is not a number or None."])
+
+        return ValidationResult(
+            is_valid=False,
+            errors=["Output data is not a number or None."]
+        )
     
 # nuke the code directory if it exists from previous runs
 import shutil
@@ -71,11 +73,12 @@ if os.path.exists(code_directory):
 autocoder = Autocoder(
     input_data="25 + 17",
     process_prompt="Generate Python code to add numbers. You must figure out how to parse the input data and return the result.",
-    max_retries=2,
+    max_retries=5,
     client=ChatGPTClientFactory.create_client(config=config, api_key=api_key),
     validator=CalculatorValidator,
     config=AutocoderConfig(
-        code_directory="/tmp/autocoder/code_generator"
+        code_directory="/tmp/autocoder/code_generator",
+        
     ),
 )
     
